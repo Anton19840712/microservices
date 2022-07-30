7742dbd649c9ec9b0281a66e8b9505e9fc0eeaa6
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-07-30 03:20:06 +0300

    Add initial aspects for ms.

---
+++ EShop/EShop.ApiGateWay/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using EShop.Infrastructure.Command.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EShop.ApiGateWay.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ProductController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> Get([FromForm] CreateProduct product)
+		{
+			await Task.CompletedTask;
+			return Accepted("Get product method called successfully!");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add([FromForm] CreateProduct product)
+		{
+			await Task.CompletedTask;
+			return Accepted("Product created successfully!");
+		}
+	}
+}
