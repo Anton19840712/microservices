b901007395880fc5bb1913cdc9509989ea446fbd
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-07-31 20:07:56 +0300

    Install masstransit package.

---
+++ EShop/EShop.Infrastructure/EventBus/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EShop.Infrastructure.EventBus
+{
+	public static class Extension
+	{
+		public static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+		{
+			var rabbitMq = new RabbitMqOption();
+			configuration.GetSection("rabbitmq").Bind(rabbitMq);
+
+			return default;
+		}
+	}
+}
---
+++ EShop/EShop.Infrastructure/EventBus/RabbitMqOption.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Infrastructure.EventBus
+{
+	public class RabbitMqOption
+	{
+		public string ConnectionString { get; set; }
+
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+	}
+}
